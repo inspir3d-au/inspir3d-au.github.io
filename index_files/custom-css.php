021878fcc0b13dc1c4b657aae95f8a3387bcc71b
David Peterson <david@randombits.org>
2017-03-05 17:45:14 +1000

    Direct port from the WordPress site.

---
+++ index_files/custom-css.php
@@ -0,0 +1,94 @@
+
+/**** GENERAL ****/
+a:hover, a:focus{
+	color:#e20019
+}
+::-webkit-input-placeholder { /* WebKit browsers */
+    color:    #999;
+}
+:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+    color:    #999;
+    opacity:  1;
+}
+::-moz-placeholder { /* Mozilla Firefox 19+ */
+    color:    #999;
+    opacity:  1;
+}
+:-ms-input-placeholder { /* Internet Explorer 10+ */
+    color:    #999;
+}
+/**** PRELOADING ****/
+.mask-color, .social-share ul.social li:hover a, #showRightPush:hover, .slicknav_btn:hover, .slicknav_nav a:hover, .slicknav_nav .slicknav_item:hover {
+	background-color:#e20019;
+}
+
+.prev:hover, .next:hover, .close-port:hover i, .read-more:hover, .read-more i, .view-all-blog:hover, .view-all-blog:hover span i, a.et-like-post.active span i, .btn.get-in-touch:hover, .et-post-data-left.single-blog a.home-icon:hover i, .form-submit input[type="submit"]:hover, .social-footer li a:hover i, .contact-form-wrapper input[type="submit"]:hover, #test_content .item .name-client, h1.title-blog a:hover, .btn.btn-oe:hover{
+	color:#e20019;
+}
+.close-port:hover, .view-all-blog:hover span, .btn.get-in-touch:hover, .image-blog-wrapper, .et-post-data-left.single-blog a.home-icon:hover, .form-submit input[type="submit"]:hover, .contact-form-wrapper input[type="submit"]:hover, .btn.btn-oe:hover, .popup-video:hover .icon-play-video{
+	border-color:#e20019;
+}
+.line-wrapper .line-circle, .pie-content h2, .percent-chart, .et-post-month, .et-post-date, .title-blog, .counter .timer, .btn.get-in-touch, .title-single, .post-tags a, .oe-comments-title, .copyright, .contact-form-wrapper input[type="submit"], .text-slider, a#scroll_to, .quote-charater, #test_content .item .name-client, .slicknav_menu  .slicknav_menutxt, .slicknav_nav a, .form-submit input[type="submit"], .btn.btn-oe, .blog-filer ul li a{
+	font-weight:700;
+}
+.color-white *{
+	color:#ffffff !important;
+}
+.color-white .popup-video:hover, .color-white .popup-video:hover .icon-play-video i{
+	color:#e20019 !important;
+}
+
+                #header{
+margin: 0 auto;
+}
+
+/*portfolio hover color - yellow rgba(231,233,23,.8), red rgba(226,0,25,.8)*/
+#portfolio_list div.item a div.hover{
+    background:rgba(226,0,25,.9);
+}
+/*services icon border color*/
+#services .list-services-wrapper .service-wrapper .icon{
+    border-color:black;
+}
+
+/*blog underline color*/
+.close-port:hover, .view-all-blog:hover span, .btn.get-in-touch:hover, .image-blog-wrapper, .et-post-data-left.single-blog a.home-icon:hover, .form-submit input[type="submit"]:hover, .contact-form-wrapper input[type="submit"]:hover, .btn.btn-oe:hover, .popup-video:hover .icon-play-video{
+    border-color:#e20019;
+}
+
+/*portfolio active category color*/
+ul.portfolio-category li a:hover span.icon-categories, ul.portfolio-category li a.active span.icon-categories{
+    border-color:rgb(226,0,25);
+}
+
+/*services icon border color
+.list-services-wrapper .service-wrapper .icon{
+    border-color:white;
+}
+*/
+
+/*background loading screen color*/
+.mask-color, .social-share ul.social li:hover a, #showRightPush:hover, .slicknav_btn:hover, .slicknav_nav a:hover, .slicknav_nav .slicknav_item:hover{
+    background-color:rgb(226,0,25);
+}
+
+/*removing 'Get In Touch' from header*/
+.contact-form-wrapper h2.contact-title{
+    display:none;
+}
+
+/*sent message*/
+div.wpcf7-response-output{
+    margin:4em 0.5em 1em;
+    background:white;
+}
+
+/*social icons centering*/
+.social-footer{
+    margin:0px 0px 0px 15px;
+}
+
+/*contact form - removing space*/
+.list-contact-wrapper{
+    margin-top:0px;
+}            
